3bf75027f289d9d28cd9f33f15405f4c078b351a
Janne Tolppanen <janne.ks@gmail.com>
2023-06-02 17:27:44 +0300

    5.1 added login form component. does not do anything

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,18 +1,33 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import LoginForm from './components/LoginForm'
+
 
 const App = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
   const [blogs, setBlogs] = useState([])
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
 
   return (
     <div>
+      <LoginForm
+        handleLogin={handleLogin}
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword} />
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
