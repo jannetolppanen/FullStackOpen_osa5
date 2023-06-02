f15df5ad8cf2611ba3e001a23d80435ea3ab6f46
Janne Tolppanen <janne.ks@gmail.com>
2023-06-02 22:46:41 +0300

    5.1 done. frontend can login and rendering loginform and blogs are conditional based on user logged in

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 import LoginForm from './components/LoginForm'
 
 
@@ -8,6 +9,7 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -15,23 +17,43 @@ const App = () => {
     )
   }, [])
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
-    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('wrong creds')
+      // setErrorMessage('wrong credentials')
+      // setTimeout(() => {
+      //   setErrorMessage(null)
+      // }, 5000)
+    }
   }
 
   return (
     <div>
+      {!user && (
       <LoginForm
-        handleLogin={handleLogin}
-        username={username}
-        setUsername={setUsername}
-        password={password}
-        setPassword={setPassword} />
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      handleLogin={handleLogin}
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword} />
       )}
+
+{user && (
+  <div>
+    <h2>blogs</h2>
+    {blogs.map(blog => (
+      <Blog key={blog.id} blog={blog} />
+    ))}
+  </div>
+)}
     </div>
   )
 }
